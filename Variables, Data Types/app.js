a3ef60b08fffe6784dee43a982adcbc0184bc4ab
Serhat GECGIN <74310970+gecginserhat@users.noreply.github.com>
2024-09-04 20:15:55 +0300

    Add files via upload

---
+++ Variables, Data Types/app.js
@@ -0,0 +1,99 @@
+//Variables and Data Types
+
+// var a = 20;
+// var b =10;
+// var c = 40;
+
+// console.log(a,b,c );
+
+
+
+//Primitive types
+
+//Number
+/*
+var a = 10;//Data Types Number
+var b = 3.14;//Data Types Number
+console.log(typeof a);
+console.log(typeof b);
+*/
+
+//String
+/*
+var name = "Serhat";
+
+console.log(name);
+console.log(typeof name);
+*/
+
+//Boolean
+/*
+var a = true;
+console.log(a);
+console.log(typeof a);
+*/
+
+//Null
+/*
+var a = null;
+console.log(a);
+console.log(typeof a);
+*/
+
+//Undefined
+/*
+var a;
+console.log(a);
+*/
+
+
+
+//Reference types
+
+//Object
+/*
+var numbers = [1,2,3,4,5];
+console.log(numbers);
+console.log(typeof numbers);
+console.log(numbers[0]);
+*/
+
+/*
+var person = {
+    name: "Serhat",
+    age: 23
+}
+console.log(person);
+console.log(typeof person);
+*/
+
+/*
+var date = new Date();
+console.log(date);
+console.log(typeof date);
+*/
+
+//Funciton
+/*
+var merhaba = function(){
+    console.log("Merhaba");
+}
+console.log(merhaba);
+console.log(typeof merhaba);
+*/
+
+/*
+var a = 10;
+var b = a;
+console.log(a,b);
+a=20;
+console.log(a,b);
+*/
+
+
+var a = [1,2,3];
+var b = a;
+a.push(4);
+console.log(b);
+
+
