65a17ebe674a19c29fa2871ae0402f21ffbd45ea
Serhat GECGIN <74310970+gecginserhat@users.noreply.github.com>
2024-09-04 20:21:11 +0300

    Add files via upload

---
+++ 6.Operators and Math Object/app.js
@@ -0,0 +1,47 @@
+let value;
+
+const value1 = 10;
+const value2 = 4;
+
+//Arithmetic Operations
+
+/*
+value = value1 + value2;
+value = value1 - value2;
+value = value1 * value2;
+value = value1 / value2;
+value = value1 % value2;
+*/
+
+value = Math.PI;
+value = Math.E;
+
+//round down
+value = Math.round(3.6);
+value = Math.round(3.5);
+value = Math.round(3.4);
+
+
+//round up
+value = Math.ceil(3.2);
+
+
+value= Math.floor(3.2);
+value = Math.floor(3.7);
+
+
+value = Math.sqrt(25);
+value = Math.sqrt(16);
+
+value = Math.abs(-10);
+
+value = Math.pow(8,3);
+value = Math.pow(4,2);
+
+value = Math.max(10,-1,100,32);
+value = Math.min(10,-1,100,32);
+
+value = Math.random() * 10;
+value = Math.floor(Math.random() * 20 + 1);
+
+console.log(value);
