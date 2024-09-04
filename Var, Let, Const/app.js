5e82c34dce01c697989c2e3180908515c006ed49
Serhat GECGIN <74310970+gecginserhat@users.noreply.github.com>
2024-09-04 20:19:21 +0300

    Add files via upload

---
+++ Var, Let, Const/app.js
@@ -0,0 +1,60 @@
+//Var
+/*
+var a;
+a = 20; 
+console.log(a);
+*/
+
+/*
+var name = "Serhat Geçgin";
+console.log(name);
+
+name = "Serhat";
+console.log(name);
+*/
+
+//Let
+/*
+let name = "Serhat Geçgin";
+console.log(name);
+name = "Serhat";
+console.log(name);
+*/
+
+/*
+let a,b;
+a=10;
+b=20;
+console.log(a+b);
+*/
+
+//Const
+//ıf we write like that we take error messages
+/*
+const name = "Serhat Geçgin";
+console.log(name);
+name = "Serhat";
+console.log(name);
+*/
+
+//we use const in 1 line. If we use like that we take error messages
+/*
+const a;
+a=10;
+console.log(a);
+*/
+
+//this usage not take error. Array
+const a = [1,2,3,4,5];
+console.log(a);
+a.push(6);
+console.log(a);
+
+
+
+
+
+
+
+
+
